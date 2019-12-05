0245dd739e3587e03e37868905ad0b654623166f
Amelia Woodward <ameliawd@stanford.edu>
2019-12-05 00:51:40 -0800

    Colocalization functionality added to puncta_count.m

--- puncta_count.m
+++ puncta_count.m
@@ -202,3 +202,10 @@ elseif strcmpi(str, 'blue')
     ylabel('number of puncta');
 end
 
+ %Colocalization of puncta 
+ imwrite(RC, 'red.tif')
+ imwrite(GC, 'green.tif')
+ imwrite(BC, 'blue.tif')
+    
+ scatterOutput = CLC('red.tif','green.tif',[],2,'hot',1,1); 
+
