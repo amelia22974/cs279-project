51dcc4eb7df0b1747ad6c7992a23b6f8f686efb8
Amelia Woodward <ameliawd@stanford.edu>
2019-12-04 17:54:49 -0800

    Cleaned up geodesic.m

--- geodesic.m
+++ geodesic.m
@@ -2,21 +2,18 @@
 % So, now we have this really basic information about the images
 % Our next step is to idenitfy dendrites with considerable accuracy.
 % We first do some simple processing on the image to maximise sensitivity. 
-clear all 
-close all
-
 A = imgetfile()
 img = imread(A);
 img= imsharpen(img, 'Radius', 4, 'Amount', 1.5);
 A = img;
 imshow(img);
 
-color = menu('What color are the puncta you wish to quantify?','Red','Green', 'Blue');
+color = menu('What color are the puncta you wish to quantify?','Red', 'Green','Blue');
 
 %Select your channels here. We've eliminates irrelevant ones. This is done
 %manually as of now
 if color == 1
-    m= 1;
+    m = 1;
     n = 2
     o =3
 elseif color == 2
@@ -31,35 +28,48 @@ end
 A(:,:,n)=0;
 A(:,:,o) = 0;
 
-msgbox('Running a bunch of processing steps --> be patient!')
+h = 0;
+h = menu('The image is about to process --> be patient! We will let you know as processes are completed.', 'CONTINUE');
+while(h == 0)
+    wait
+    imclose(A)
+end
 
 % Here we do a k means based segmentation using 6 clusters 
 [L,C] = imsegkmeans(A,6);
 A = label2rgb(L,im2double(C));
-%imshow(A)
-hold on 
-close all
-   
+h = 0;
+h = menu('Finished kmeans. Now we will apply fibermetric.', 'CONTINUE');
+
+while(h == 0)
+    wait
+    imclose(A)
+end
+
 %Multiple iterations of gaussian filtering at a low SD to preserve edges we want 
 % but eliminate all the noise in the image.
 for i =1:1000
     A = imgaussfilt(A,1);
 end
-%imshow(A)
 
 %Use the fibermetric algorithm to identify dendrites based on tubularity and then process 
 % image accordingly. This can take a while to run 
-
 C=A;
 A = fibermetric(A,'ObjectPolarity','bright');
 A(~A) = true;
+
+h = 0;
+h = menu('Finished fibermetric. Now we skeletonize and run the geodesic transform.', 'CONTINUE');
+while(h == 0)
+    wait
+    imclose(A)
+end
 
 %Here we threshold our images 
 thresh = graythresh(A(:,:,m));
 A = imbinarize(A(:,:,m), thresh);
 A=imfill(A, 16);
 A = imcomplement(A);
-
 
 %Creates a structural object and uses it to dilate the image
 se = strel('disk', 1, 4);
@@ -76,28 +86,25 @@ E = bwmorph(skel, 'endpoints');
 [y,x] = find(E);
 B_loc = find(B);
 Dmask = false(size(skel));
+
 for k = 1:numel(x)
     D = bwdistgeodesic(skel,x(k),y(k));
     distanceToBranchPt = min(D(B_loc));
     Dmask(D < distanceToBranchPt) =true;
 end
 skelD = skel - Dmask;
-imshow(skelD);
 
 % A bunch of other processing to get rid of spurious dendrites that don't
 % fit our criteria
-figure(6)
 skelD = bwmorph(skelD, 'close', 10);
 skelD = bwmorph(skelD, 'fill', 10);
 skelD = bwmorph(skelD, 'bridge', 5);
 skelD = bwmorph(skelD, 'spur');
 skelD = bwmorph(skelD, 'hbreak');
-title('Here is a comparison between our final map and the original image')
-%imshowpair(skelD, img, 'montage');
+
 
 % Let's also get some useful data out of this
 B = bwmorph(skelD, 'branchpoints');
-figure, imshowpair(B, skelD, 'montage');
 B = bwmorph(B, 'close', 10);
 B = bwmorph(B, 'fill', 10);
 B = bwmorph(B, 'bridge', 5);
@@ -116,22 +123,26 @@ disp(sum(logical==1));
 
 % Our next goal is to find some way of quanitfying dendiritic arborization in a way that might be useful to researchers 
 while(1)
-    %neuriteask=input('Do you want to select a neurite?, Y/N [Y]: ','s')
-    neuritetask = menu('Do you want to select a neurite?', 'Yes', 'No')
-    scale = menu('Do you want to select a scale factor', 'default')
+    
+    neuritetask = menu('Do you want to select a neurite? Only select along the neurite skeleton! Length will appear in your command line.', 'Yes', 'No')
+    
+    %Close the program if the user doesn't want to quantify the length of
+    %any more neurites.
+    if neuritetask == 2
+        return;
+    end
+   
+    %CHANGE THIS SCALE FACTOR IF YOU NEED A DIFFERENT ONE
+    scale = 1;
+    
+    img = imshow(skelD);
+    title("Drag the mouse and double click to end selection.");
+    roi = drawassisted(img,'Color','r', 'Closed', false);
+    skeletonmask = createMask(roi);
+    imshow(skeletonmask);
+    statsmask = regionprops(skeletonmask, 'MajorAxis', 'Perimeter');
 
-if neuritetask == 0
-break
-end
-
-%figure(10)
-img = imshow(skelD);
-roi = drawassisted(img,'Color','r', 'Closed', false);
-skeletonmask = createMask(roi);
-imshow(skeletonmask);
-statsmask = regionprops(skeletonmask, 'MajorAxis', 'Perimeter');
-  
-Neurite_length1 = statsmask.Perimeter/2 .* scale
+    Neurite_length1 = statsmask.Perimeter/2 .* scale
 
 end
 
@@ -142,60 +153,56 @@ myData.Units = 'pixels';
 myData.MaxValue = hypot(sz(1),sz(2));
 myData.Colormap = hot;
 myData.ScaleFactor = 1;
-
-hIm = imshow(skelD);
-
 hIm.ButtonDownFcn = @(~,~) startDrawing(hIm.Parent,myData);
+
 function startDrawing(hAx,myData)
+    % Create a line ROI object. Specify the initial color of the line and
+    % store the |myData| structure in the |UserData| property of the ROI.
+    h = images.roi.Line('Color',[0, 0, 0.5625],'UserData',myData);
 
-% Create a line ROI object. Specify the initial color of the line and
-% store the |myData| structure in the |UserData| property of the ROI.
-h = images.roi.Line('Color',[0, 0, 0.5625],'UserData',myData);
+    % Set up a listener for movement of the line ROI. When the line ROI moves,
+    % the |updateLabel| callback updates the text in the line ROI label and
+    % changes the color of the line, based on its length.
+    addlistener(h,'MovingROI',@updateLabel);
 
-% Set up a listener for movement of the line ROI. When the line ROI moves,
-% the |updateLabel| callback updates the text in the line ROI label and
-% changes the color of the line, based on its length.
-addlistener(h,'MovingROI',@updateLabel);
+    % Set up a listener for clicks on the line ROI. When you click on the line
+    % ROI, the |updateUnits| callback opens a GUI that lets you specify the
+    % known distance in real-world units, such as, meters or feet.
+    addlistener(h,'ROIClicked',@updateUnits);
 
-% Set up a listener for clicks on the line ROI. When you click on the line
-% ROI, the |updateUnits| callback opens a GUI that lets you specify the
-% known distance in real-world units, such as, meters or feet.
-addlistener(h,'ROIClicked',@updateUnits);
+    % Get the current mouse location from the |CurrentPoint| property of the
+    % axes and extract the _x_ and _y_ coordinates.
+    cp = hAx.CurrentPoint;
+    cp = [cp(1,1) cp(1,2)];
 
-% Get the current mouse location from the |CurrentPoint| property of the
-% axes and extract the _x_ and _y_ coordinates.
-cp = hAx.CurrentPoint;
-cp = [cp(1,1) cp(1,2)];
+    % Begin drawing the ROI from the current mouse location. Using the
+    % |beginDrawingFromPoint| method, you can draw multiple ROIs.
+    h.beginDrawingFromPoint(cp);
 
-% Begin drawing the ROI from the current mouse location. Using the
-% |beginDrawingFromPoint| method, you can draw multiple ROIs.
-h.beginDrawingFromPoint(cp);
-
-% Add a custom option to the line ROI context menu to delete all existing
-% line ROIs.
-c = h.UIContextMenu;
-uimenu(c,'Label','Delete All','Callback',@deleteAll);
+    % Add a custom option to the line ROI context menu to delete all existing
+    % line ROIs.
+    c = h.UIContextMenu;
+    uimenu(c,'Label','Delete All','Callback',@deleteAll);
 
 end
 
 function updateLabel(src,evt)
+    % Get the current line position.
+    pos = evt.Source.Position;
 
-% Get the current line position.
-pos = evt.Source.Position;
+    % Determine the length of the line.
+    diffPos = diff(pos);
+    mag = hypot(diffPos(1),diffPos(2));
 
-% Determine the length of the line.
-diffPos = diff(pos);
-mag = hypot(diffPos(1),diffPos(2));
+    % Choose a color from the color map based on the length of the line. The
+    % line changes color as it gets longer or shorter.
+    color = src.UserData.Colormap(ceil(64*(mag/src.UserData.MaxValue)),:);
 
-% Choose a color from the color map based on the length of the line. The
-% line changes color as it gets longer or shorter.
-color = src.UserData.Colormap(ceil(64*(mag/src.UserData.MaxValue)),:);
+    % Apply the scale factor to line length to calibrate the measurements.
+    mag = mag*src.UserData.ScaleFactor;
 
-% Apply the scale factor to line length to calibrate the measurements.
-mag = mag*src.UserData.ScaleFactor;
-
-% Update the label.
-set(src,'Label',[num2str(mag,'%30.1f') ' ' src.UserData.Units],'Color',color);
+    % Update the label.
+    set(src,'Label',[num2str(mag,'%30.1f') ' ' src.UserData.Units],'Color',color);
 
 end
 
@@ -256,7 +263,6 @@ end
 
 end
 
-%%
 function allevents(src,evt)
     evname = evt.EventName;
     switch(evname)
@@ -268,25 +274,3 @@ function allevents(src,evt)
             disp(['ROI moved current position: ' mat2str(evt.CurrentPosition)]);
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
