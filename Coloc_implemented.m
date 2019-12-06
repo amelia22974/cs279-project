d63637e7de44397b22cc78cb55ae9ef9e17c6cb6
sidsd27 <58278977+sidsd27@users.noreply.github.com>
2019-12-06 12:56:42 -0800

    Delete Coloc_implemented.m

--- Coloc_implemented.m
+++
@@ -1,7 +0,0 @@
- %% Colocalization of puncta 
-    imwrite(RC, 'red.tif')
-    imwrite(GC, 'green.tif')
-    imwrite(BC, 'blue.tif')
-    
-    scatterOutput = CLC('red.tif','green.tif',[],2,'hot',1,1); 
-    
